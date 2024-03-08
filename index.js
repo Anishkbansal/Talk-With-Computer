6861c8f2b772c0522b8f4fd791d1800411242940
Anishkbansal <Anishkbansal2007@gmail.com>
2024-03-08 14:08:28 +0530

    added ide iframe

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ window.addEventListener('scroll', () => {
     progressBar.style.filter = `grayscale(${100 - scrollProgress}%)`;
     progressBar.style.clipPath = `inset(${100 - scrollProgress}% 0 0 0)`;
   });
+
+
+  var toggleBtn = document.querySelectorAll('.toggleBtn');
+  var iframeContainer = document.querySelectorAll('.iframeContainer');
+  var iframeSrc = 'https://anishbansal.pythonanywhere.com/'
+  
+  toggleBtn.forEach(function(btn, index){
+    btn.addEventListener('click', function(){
+      toggleIframe(index);
+    });
+  });
+
+  function toggleIframe(index){
+    iframeContainer.forEach(function(container, i){
+      if(i === index){
+        if(container.style.display === 'none'){
+          container.style.display = 'block';
+          var iframe = container.querySelector('iframe');
+          iframe.setAttribute('src', iframeSrc);
+          container.scrollIntoView({behavior:'smooth', block: 'center'})
+
+        }else{
+          container.style.display = 'none';
+        }
+      }else{
+        container.style.display = 'none'
+      }
+    });
+  }
